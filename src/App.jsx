af68d98f15426c06de82afb2cf1b86fbdee9440c
Full-Stack-Foundations <131329832+Yash-Devs@users.noreply.github.com>
2026-06-22 17:47:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>React GitHub Pages Demo</h1>
+
+      <p>
+        This application is automatically deployed using GitHub Actions.
+      </p>
+
+      <button
+        onClick={() => alert("Deployment Working!")}
+      >
+        Test Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
