1e38454f7f8a371575054d4ff0bdcf607f5b4280
Full-Stack-Foundations <131329832+Yash-Devs@users.noreply.github.com>
2026-06-22 17:58:16 +0530

    code update version 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1>React GitHub Pages Demo</h1>
-
+      <h1>Version 2</h1>
       <p>
         This application is automatically deployed using GitHub Actions.
       </p>
